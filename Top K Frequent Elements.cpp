4016fb2e4363d24303fc4cc55e1e7921326645fa
ishaan112233 <ishaans937@gmail.com>
2020-07-17 18:35:15 +0530

    Day17

---
+++ Top K Frequent Elements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        if(nums.empty()){
+            return {};
+        }    
+        unordered_map<int,int>mp;
+        for(auto i: nums){
+            mp[i]++;
+        }
+        vector<int>ans;
+        priority_queue<pair<int,int>>pq;
+        for(auto i: mp){
+            pq.push({i.second, i.first});
+            if(pq.size() > mp.size() - k){
+                ans.emplace_back(pq.top().second);
+                pq.pop();
+            }
+        }
+        return ans;
+    }
+};
